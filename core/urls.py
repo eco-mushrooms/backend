bc41985ad09150de4324920e4672b5a86274ef6e
Rontim <rongitonga@gmail.com>
2024-07-05 22:41:02 +0300

    chore: Update .dockerignore and core/urls.py, add health check endpoints

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mushroom/', include('mushroom.urls')),
+    path('health-check/', health_check),
 ]
