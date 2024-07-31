6e2baca29cadbcc161ffc13bf002f6fa5ea4b0df
Rontim <rongitonga@gmail.com>
2024-07-31 11:39:33 +0300

    chore: Add room creation endpoint to farm URLs

--- farm/urls.py
+++ farm/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import FarmCreateView
+from .views import FarmCreateView, RoomCreateView
 
 urlpatterns = [
     path('create', FarmCreateView.as_view(), name='farm-create'),
+    path('room/create', RoomCreateView.as_view(), name='room-create'),
 ]
--- farm/views.py
+++ farm/views.py
@@ -1,10 +1,11 @@
 from drf_yasg.utils import swagger_auto_schema
-
+from drf_yasg import openapi
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 
 from farm.models import Farm, Room, Location
 from .serializers import (FarmSerializer, LocationSerializer, RoomSerializer, CO2SensorSerializer, LightSensorSerializer,
@@ -19,8 +20,34 @@ class FarmCreateView(APIView):
     permission_classes = [IsAuthenticated]
 
     @swagger_auto_schema(
-        request_body=FarmSerializer,
-        responses={status.HTTP_201_CREATED: FarmSerializer},
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'name': openapi.Schema(type=openapi.TYPE_STRING, description='Name of the farm'),
+                'location': openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'latitude': openapi.Schema(type=openapi.TYPE_NUMBER, description='Latitude of the farm'),
+                        'longitude': openapi.Schema(type=openapi.TYPE_NUMBER, description='Longitude of the farm')
+                    }
+                )
+            }
+        ),
+        responses={status.HTTP_201_CREATED: openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Farm ID'),
+                'name': openapi.Schema(type=openapi.TYPE_STRING, description='Name of the farm'),
+                'location': openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'latitude': openapi.Schema(type=openapi.TYPE_NUMBER, description='Latitude of the farm'),
+                        'longitude': openapi.Schema(type=openapi.TYPE_NUMBER, description='Longitude of the farm')
+                    }
+                )
+            },
+        )
+        },
         operation_description='Create a new farm'
     )
     def post(self, request):
@@ -33,12 +60,33 @@ class FarmCreateView(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class RoomCreateView(APIView):
+class RoomCreateView(CreateAPIView):
     permission_classes = [IsAuthenticated]
+    serializer_class = RoomSerializer
 
-    def post(self, request):
-        room = request.data
-        serializer = RoomSerializer(data=room)
-        serializer.is_valid(raise_exception=True)
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    @ swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'name': openapi.Schema(type=openapi.TYPE_STRING, description='Name of the room'),
+                'mushroom_variety': openapi.Schema(type=openapi.TYPE_STRING, description='Type of mushroom'),
+                'no_of_bags': openapi.Schema(type=openapi.TYPE_INTEGER, description='Number of bags in the room'),
+                'farm': openapi.Schema(type=openapi.TYPE_INTEGER, description='Farm ID')
+            }
+        ),
+        responses={
+            status.HTTP_201_CREATED: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Room ID'),
+                    'name': openapi.Schema(type=openapi.TYPE_STRING, description='Name of the room'),
+                    'mushroom_variety': openapi.Schema(type=openapi.TYPE_STRING, description='Type of mushroom'),
+                    'no_of_bags': openapi.Schema(type=openapi.TYPE_INTEGER, description='Number of bags in the room'),
+                    'farm': openapi.Schema(type=openapi.TYPE_INTEGER, description='Farm ID')
+                }
+            )
+        },
+        operation_description='Create a new room'
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
