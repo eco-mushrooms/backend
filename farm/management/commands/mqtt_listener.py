d020ed93f3aa7bb4cf1a4d521416ca7236ec9979
Rontim <rongitonga@gmail.com>
2024-08-02 15:22:21 +0300

    chore: Update MQTT listener to handle sensor and actuator messages

--- farm/management/commands/mqtt_listener.py
+++ farm/management/commands/mqtt_listener.py
@@ -3,6 +3,7 @@ import sys
 import time
 import paho.mqtt.client as mqtt
 from django.core.management.base import BaseCommand
+from channels.layers import get_channel_layer
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -22,6 +23,8 @@ class ReloadHandler(FileSystemEventHandler):
 
 class Command(BaseCommand):
     help = 'Starts an MQTT listener with autoreload functionality.'
+    username = os.environ.get('MQTT_USERNAME', '')
+    password = os.environ.get('MQTT_PASSWORD', '')
 
     def handle(self, *args, **options):
         self.should_reload = False
@@ -47,11 +50,13 @@ class Command(BaseCommand):
         os.execv(sys.executable, ['python'] + sys.argv)
 
     def run_mqtt_listener(self):
-        client = mqtt.Client()
+        client = mqtt.Client(protocol=mqtt.MQTTv5)
         client.on_connect = self.on_connect
         client.on_message = self.on_message
 
         try:
+            client.username_pw_set(
+                username=self.username, password=self.password)
             client.connect(MQTT_BROKER, MQTT_PORT, 60)
             client.loop_start()
         except Exception as e:
@@ -60,7 +65,24 @@ class Command(BaseCommand):
 
     def on_connect(self, client, userdata, flags, rc):
         print(f"Connected with result code {rc}")
-        client.subscribe("your/topic")
+        client.subscribe("sensor/monitor")
+        client.subscribe("actuator/monitor")
 
     def on_message(self, client, userdata, msg):
         print(f"{msg.topic} {msg.payload}")
+        if msg.topic == "sensor/monitor":
+            channel_layer = get_channel_layer()
+            channel_layer.group_send(
+                'sensor_monitor', {
+                    'type': 'sensor_monitor',
+                    'message': msg.payload.decode()
+                }
+            )
+        elif msg.topic == "actuator/monitor":
+            channel_layer = get_channel_layer()
+            channel_layer.group_send(
+                'actuator_monitor', {
+                    'type': 'actuator_monitor',
+                    'message': msg.payload.decode()
+                }
+            )
